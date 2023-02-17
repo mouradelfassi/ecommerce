1510d3c4c0e360744fcb5ddf8968d0f00a4c5341
mourad <miurad@gmail.com>
2023-02-17 04:00:25 +0100

    initial commit

---
+++ ecommerceapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Contac,  Conttac, Product, Orders, OrderUpdate
+# Register your models here
+admin.site.register(Contac)
+admin.site.register(Conttac)
+admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+
+
